582c79de8ca2d7c8e44adc26ab946f64877afac6
Blessed Sibanda <bladesibanda@gmail.com>
2023-01-23 16:41:46 +0200

    refactor tests

--- src/test/java/com/polarbookshop/orderservice/order/domain/OrderRepositoryR2dbcTests.java
+++ src/test/java/com/polarbookshop/orderservice/order/domain/OrderRepositoryR2dbcTests.java
@@ -1,43 +1,16 @@
 package com.polarbookshop.orderservice.order.domain;
 
-import com.polarbookshop.orderservice.config.DataConfig;
+import com.polarbookshop.orderservice.PostgreSQLContainerTestBase;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 import reactor.test.StepVerifier;
 
 @DataR2dbcTest
-@Import(DataConfig.class)
-@Testcontainers
-public class OrderRepositoryR2dbcTests {
-    @Container
-    static PostgreSQLContainer<?> postgresql =
-            new PostgreSQLContainer<>(DockerImageName.parse("postgres:14.4"));
-
+public class OrderRepositoryR2dbcTests extends PostgreSQLContainerTestBase {
     @Autowired
     private OrderRepository orderRepository;
 
-    @DynamicPropertySource
-    static void postgresqlProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.r2dbc.url", OrderRepositoryR2dbcTests::r2dbcurl);
-        registry.add("spring.r2dbc.username", postgresql::getUsername);
-        registry.add("spring.r2dbc.password", postgresql::getPassword);
-        registry.add("spring.flyway.url", postgresql::getJdbcUrl);
-    }
-
-    private static String r2dbcurl() {
-        return String.format("r2dbc:postgresql://%s:%s/%s",
-                postgresql.getContainerIpAddress(),
-                postgresql.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT),
-                postgresql.getDatabaseName());
-    }
 
     @Test
     void createRejectedOrder() {
